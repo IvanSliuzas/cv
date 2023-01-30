b25057974ef9a583af0fe4ae83a230ab57797439
IvanSliuzas <123782464+IvanSliuzas@users.noreply.github.com>
2023-01-30 17:51:00 -0300

    Create cv.js

---
+++ cv.js
@@ -0,0 +1,8 @@
+const button = document.querySelector('button');
+
+      button.onclick = function(button) {
+        let name = prompt('¿Cual es tu nombre?');
+        alert('Hola '+name+'!!! Cualquier consulta que tengas, mi contacto esta al final de la pagina! O ingresa al logo de WhatsApp en la esquina inferior derecha.');
+      }
+
+      
